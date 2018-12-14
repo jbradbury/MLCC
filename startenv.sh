f5940f11269ddef4344ed139913fbda8d790bd12
jbradbury <bradburyj@protonmail.com>
2018-12-14 12:16:07 +0000

    updated startenv.sh

--- startenv.sh
+++ startenv.sh
@@ -1,3 +1,4 @@
+virtualenv --no-site-package jupyter-env
 source jupyter-env/bin/activate
 pip install  --ignore-installed --upgrade \
   tensorflow "matplotlib<3" pandas sklearn scipy seaborn ipython==5.7 notebook
